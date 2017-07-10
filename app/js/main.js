d328eeec9fa6e04847ed339642fa09e72d8c582b
Alex <ahysov.alex@gmail.com>
2017-07-11 01:06:48 +0300

    full responsive

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,27 @@ $(document).ready(function(){
 	$('.section-banner__slider').slick({
 		dots: false,
 		responsive: [{
-			breakpoint: 481,
+			breakpoint: 769,
 			settings: {
 				arrows: false,
 				dots:true
 			}
 		}]
 	});
+
+	/////////
+
+	/*$('.header').removeClass('h-scroll');
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.header').addClass('h-scroll').fadeIn('fast');
+			$('.wrapper').css('margin-top', '100px');
+		} else {
+			$('.header').removeClass('h-scroll').fadeIn('fast');
+			$('.wrapper').css('margin-top', '0');
+		};
+	});*/
+
+	////////
 
 });
